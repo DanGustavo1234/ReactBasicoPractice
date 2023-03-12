dd4616cf3aaf8e1c92d20af86cc327c39b085e67
@DanGustavo1234 <dacordova1234@gmail.com>
2023-03-12 11:27:44 -0500

    three commit

---
+++ src/TodoList.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+const todos=[{text:"Cut onion",complete:false},
+{text:"Hello",complete:false},
+{text:"Bye",complete:false}];
+
+export default function TodoList(todos) {
+  return (
+    <div> 
+    <h1>Hello</h1>
+    {/* {todos.map(todo=> (
+      <h1>Hello</h1>
+    ))} */}
+  
+  </div>
+  )
+}
---
+++ src/TodoSearch.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function TodoSearch() {
+  return (
+    <div> <input placeholder="Onion"/></div>
+  )
+}
